bae7eb91e5e255e62b950a082ba3e84fc019bb8d
M-Garrison <103372479+M-Garrison@users.noreply.github.com>
2022-09-13 11:00:49 -0500

    changes to app.js

---
+++ src/components/ShowKeyboard0.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const ShowKeyboard0 = () => {
+    return (
+        <div>
+            <h1>Keyboard Table:</h1>
+        </div>
+    )
+}
+
+export default ShowKeyboard0
