0283b2c5fc25aefafa536056b6c49c9e3fee50b8
eduardopaaaa <eddy.nator@hotmail.com>
2022-09-13 14:06:35 -0600

    pushing GS, footer, header, sbar, search

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = (props) => {
+	return (
+		<div className='nav nav-masthead justify-content-center float-md'>
+			<footer className='mt-auto'>
+			<p>Keyboard Part Picker™ | Deigned & Built in Timbuktu</p>
+				
+
+						
+				
+			</footer>
+		</div>
+	);
+};
+
+export default Footer;
