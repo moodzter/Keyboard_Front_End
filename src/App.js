e179dfdc61a91f44515ea0eda1844cd103bdb220
Nick Moody <Nicholasmoodz1543@gmail.com>
2022-09-13 08:38:58 -0700

    Made the Create component and linked it to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import CreateKeyboard from './components/CreateKeyboard'
 
 
 function App() {
   return (
     <div>
       <h1>Hello World!!!!</h1>
-      </div>
+      <CreateKeyboard/>
+    </div>
   );
 }
 
