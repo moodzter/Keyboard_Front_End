72ce8b6ba84f40fe9f70ba3ea7592cab6fb03284
Nick Moody <Nicholasmoodz1543@gmail.com>
2022-09-14 16:59:11 -0700

    pre-pull from dev

--- src/components/CreateKeyboard.js
+++ src/components/CreateKeyboard.js
@@ -1,17 +1,65 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
-import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { keyboard } from '@testing-library/user-event/dist/keyboard';
 
-const CreateKeyboard = () => {
+const CreateKeyboard = ({}) => {
     
+    const handleCreate = (addKeyboard) => {
+        axios
+        .post('http://localhost:8000/api/keyboards', addKeyboard)
+        .then((response) => {
+            setKeyoard([...people, response.data])
+        })
+    }
+    let emptyKeyboard = {
+        brand: '',
+        switches: '', 
+        keycaps: '', 
+        stabilizers: '',
+        price: '', 
+        size: '', 
+        img: ''
+    }
+    let [keyboard, setKeyboard] = useState(emptyKeyboard)
 
+    const handleChange = (event) => {
+        setKeyboard({ ...keyboard, [event.target.name]: event.target.value })
+    }
     return (
         <div>
-            <h1>Create Keyboard Component</h1>
-            <Button> hello </Button>
+            <form>
+                <label>Brand:</label>
+                <input type ='text' name='' onChange={handleChange}/>
+                <br/>
+                <br/>
+                <label>Switches:</label>
+                <input type ='text' name='' onChange={handleChange}/>
+                <br/>
+                <br/>
+                <label>Keycaps:</label>
+                <input type ='text' name='' onChange={handleChange}/>
+                <br/>
+                <br/>
+                <label>Stabilizers:</label>
+                <input type ='text' name='' onChange={handleChange}/>
+                <br/>
+                <br/>
+                <label>Price:</label>
+                <input type ='text' name='' onChange={handleChange}/>
+                <br/>
+                <br/>
+                <label>Size:</label>
+                <input type='text' name='' onChange={handleChange}/>
+                <br/>
+                <br/>
+                <label>IMG URL:</label>
+                <input type ='text' name='' onChange={handleChange}/>
+                <input type ='submit' onClick={handleCreate} value='SUBMIT'/>
+            </form>
         </div>
-    )
+    );
 }
+
 
 export default CreateKeyboard                          
