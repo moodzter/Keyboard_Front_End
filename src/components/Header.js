774814432913f6654161b0c5dd1b96e3391fc8d5
eduardopaaaa <eddy.nator@hotmail.com>
2022-09-14 20:22:08 -0600

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,10 +38,6 @@ const Header = ({
 							Google Search
 						</a>
 
-						<a className='nav-link fw-bold py-1 px-0' href='/sbar.html'>
-							Keyboard Database
-						</a>
-
 						<a
 							className='nav-link fw-bold py-1 px-0'
 							href='https://github.com/moodzter/Keyboard_Back_End'
