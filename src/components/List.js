04236c86dfcba354d650e67ed3ce31d78ea9e010
eduardopaaaa <eddy.nator@hotmail.com>
2022-09-16 10:36:28 -0600

    pushing list updates

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,48 @@
-import { React, useState } from 'react'
+import { React, useState, useRef } from 'react'
+
 import data from "./ListData.json"
-
-
+import axios from 'axios'
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { keyboard } from '@testing-library/user-event/dist/keyboard';
 
 function List(props) {
+
+    const selectedKeyboard = useRef()
+
+    const handleCreate = (event) => {
+        
+        axios
+        .post('http://localhost:8000/api/keyboards', selectedKeyboard)
+        .then((response) => {
+            console.log('successfully added')
+        })
+    }
+
+    const handlePotato = (nKB) => {
+        selectedKeyboard.current = nKB
+        
+    }
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault()
+    //     handleCreate(selectedKeyboard)
+    // }
+    // let emptyKeyboard = {
+    //     brand: '',
+    //     switches: '', 
+    //     keycaps: '', 
+    //     stabilizers: '',
+    //     price: '', 
+    //     size: '', 
+    //     img: ''
+    // }
+    // let [keyboard, setKeyboard] = useState(emptyKeyboard)
+
+    // const handleChange = (event) => {
+    //     setKeyboard({ ...keyboard, [event.target.name]: event.target.value })
+    // }
+
+////////////////////////////////////////////////////////////////////////
 
     const filteredData = data.filter((L) => {
         if (props.input === '') {
@@ -25,21 +64,21 @@ function List(props) {
     return (
         <ul>
             {filteredData.map((item) => (
-                <li className="justify-content-center nav nav-masthead" key={item.id}>
-                <br/>
-                Keyboard: {item.brand}
-                <br/>
-                Switch Type: {item.switches}
-                <br/>
-                Stabilizers: {item.stabilizers}
-                <br/>
-                Keycaps: {item.keycaps}
-                <br/>
-                Layout: {item.size}
-                <br/>
-                Price: {item.price}
-                <br/>
-                </li>
+                <><li className="justify-content-center nav nav-masthead" key={item.id}>
+                    <br />
+                    Keyboard: {item.brand}
+                    <br />
+                    Switch Type: {item.switches}
+                    <br />
+                    Stabilizers: {item.stabilizers}
+                    <br />
+                    Keycaps: {item.keycaps}
+                    <br />
+                    Layout: {item.size}
+                    <br />
+                    Price: {item.price}
+                    <br />
+                </li><form onSubmit={handleCreate}> <input onClick= {() => {handlePotato(item)}} type='submit'  value='Save to Builds' /></form></>
                 
                 
             ))}
