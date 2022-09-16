8eaa1c0a2d7ea62e04ead4875c5bbbeb6facac24
M-Garrison <103372479+M-Garrison@users.noreply.github.com>
2022-09-16 10:33:10 -0500

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -4,34 +4,18 @@ import CreateKeyboard from './components/CreateKeyboard';
 import ShowKeyboard0 from './components/ShowKeyboard0';
 import Header from './components/Header';
 import Footer from './components/Footer';
-// import SearchBar from './components/SBar';
-
-
-
-
-// import Button from 'react-bootstrap/Button';
 import './App.css';
-
+// import SearchBar from './components/SBar';
+// import Button from 'react-bootstrap/Button';
 
 function App() {
-  
-
-
   return (
-    
     <div className='App'>
       <Header/>
-      
-
-      <h1>
-
-        </h1>
-
       <CreateKeyboard/>
       <ShowKeyboard0/>
       <Footer/>
     </div>
-    
   );
 }
 
